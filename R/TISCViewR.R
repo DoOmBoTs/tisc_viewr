ac96e1e8b20d6febaabcdddee8965d5ae0a2a3a6
Bryan Thomas <76978628+Bryanrt-geophys@users.noreply.github.com>
2021-03-23 10:26:46 -0600

    documented file_paths(). Need to create namespace and document other packages.

---
+++ R/TISCViewR.R
@@ -0,0 +1,10 @@
+#' TISCViewR
+#'
+#' Organizes and visualizes TISC input and outputs for easy model to model comparison.
+#'
+#' @docType package
+#'
+#' @author Bryan R Thomas \email{bryanrt.bt@gmail.com}
+#'
+#' @name TISCViewR
+NULL
--- R/file_paths.R
+++ R/file_paths.R
@@ -1,7 +1,20 @@
+#' file_paths
+#'
+#'  Creates a named list of paths for each file type the user wants to use from TISC
+#'
+#'  @param file_paths list of file types from TISC the user wants to include
+#'  @param root_path the path to the root directory in which TISC models are located
+#'
+#'  @examples
+#'  paths <-  file_paths(wanted_files = c("pfl", "UNIT", "PRM"), root_path = )
+#'
+#'  @export
+
+
 file_paths <- function(wanted_files, root_path){
-  
+
   paths <- map(wanted_files, function(x){
-    
+
     if (x == "pfl") {
       dir_ls(
         path = root_path,
@@ -17,12 +30,12 @@ file_paths <- function(wanted_files, root_path){
         all = TRUE,
         regexp = sprintf("%s$", x)
       )
-    } 
-    
+    }
+
   })
-  
+
   pahts <- names(paths) <- wanted_files
-  
+
   paths
-  
+
 }
