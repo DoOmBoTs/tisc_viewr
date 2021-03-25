0cfdb6f4494aa7d188d26d308eeb7bc891915113
Bryan Thomas <76978628+Bryanrt-geophys@users.noreply.github.com>
2021-03-25 11:22:10 -0600

    added roxygen2 skeleton to functions. need to edit parameters to accept named file_paths and split functitions in utility function and plot functions into seperate files.

--- R/get_subsidence_data.R
+++ R/get_subsidence_data.R
@@ -1,11 +1,20 @@
+#' Title
+#'
+#' @param decompaction_data
+#' @param prm_files
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_subsidence_data <- function(decompaction_data, prm_files){
   # Equations ----
   #
   # z_tec Equation pulled from:
-  # 
+  #
   # BasinVis 1.0: A MATLABs-based program for sedimentary basin subsidence analysis and visualization
   # Eun Young Lee, Johannes Novotny, Michael Wagreich
-  # 
+  #
   # z_tec                    tectonic elevation w/seaLoad (m)
   # s                        decompacted lithoUnit (m)
   # w_d                      water depth (m), wrangle from .pfl (if top sed unit is below 0 (0 - top_sed_elevation) )
@@ -13,48 +22,48 @@ get_subsidence_data <- function(decompaction_data, prm_files){
   # rho_m                    density mantle (kg/m^3), extract from .prm | .UNIT == densmantle
   # rho_s                    density sediment (kg/m^3), extract from .prm | .UNIT == denssedim-sed_porosity*denswater
   # rho_w                    density water (kg/m^3), extract from .prm == denswater
-  # 
+  #
   # z_tec = s * ((rho_m - rho_s)/(rho_m - rho_w)) + w_d - del_sl * (rho_m/(rho_m-rho_w))
-  # 
+  #
   # gather TISC model inputs ----
-  
+
   prm_data <- map(prm_files, read_lines)
-  
-  rho_s <- map_dbl(prm_data, function(x){                              
+
+  rho_s <- map_dbl(prm_data, function(x){
     stringr::str_subset(string = x, pattern = "^denssedim") %>%
       stringr::str_extract(pattern = "\\d+\\.*\\d*") %>%
       as.numeric()
   })
-  
-  rho_m <- map_dbl(prm_data, function(x){                              
+
+  rho_m <- map_dbl(prm_data, function(x){
     stringr::str_subset(string = x, pattern = "^densmantle") %>%
       stringr::str_extract(pattern = "\\d+\\.*\\d*") %>%
       as.numeric()
   })
-  
+
   # Build tectonic subsidence data.frame ----
-  
+
   pmap(list(decompaction_data, rho_m, rho_s), function(x, y, z){
-    
+
     # x <- decompaction_data[[1]]
     # y <- rho_m[[1]]
     # z <- rho_s[[1]]
-    
+
     if (length(x) > 0) {
-      
+
       x %>%
         group_by(timestep) %>%
         summarize(
           compact_thk = sum(compact_thk, na.rm = TRUE),
           decompact_thk = sum(decompact_thk, na.rm = TRUE),
           water_depth = last(water_depth),
-          rho_m = y,                       
-          rho_w = 1000,                       
-          rho_s = z,                       
+          rho_m = y,
+          rho_w = 1000,
+          rho_s = z,
           sub_total = -first(unit_depth),
           sea_level = last(sea_level)) %>%
         mutate(
-          del_sl = sea_level - lag(sea_level), 
+          del_sl = sea_level - lag(sea_level),
           sub_tec = decompact_thk * ((rho_m - rho_s)/(rho_m - rho_w)) + water_depth - del_sl * (rho_m/(rho_m-rho_w))
         ) %>%
         drop_na() %>%
@@ -69,5 +78,5 @@ get_subsidence_data <- function(decompaction_data, prm_files){
         )
     }
   })
-  
+
 }
--- R/get_well_tops.R
+++ R/get_well_tops.R
@@ -1,30 +1,42 @@
+#' Title
+#'
+#' @param models_well_location
+#' @param pfl_files
+#' @param prm_files
+#' @param unit_files
+#' @param model_names
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_well_tops <- function(models_well_location, pfl_files, prm_files, unit_files, model_names){
-  
+
   # models_well_location
   # pfl_files
   # prm_files
   # unit_files
   # model_names
-  
+
   pfl_text_data <- map(model_names, ~ stringr::str_subset(string = pfl_files, pattern = .x)) %>%
     map(~ map(.x, read_lines))
-  
+
   pfl_data <- map(model_names, ~ stringr::str_subset(string = pfl_files, pattern = .x)) %>%
     map(~ map(.x, ~ as_tibble(read.table(.x, header = FALSE, sep = "\t", stringsAsFactors = FALSE))))
-  
+
   prm_data <- map(prm_files, read_lines)
-  
+
   unit_data <- map(model_names, ~ stringr::str_subset(string = unit_files, pattern = .x)) %>%
     map(~ map(.x, read_lines))
-  
+
   bedrock_density <- map_dbl(prm_data, function(x){
     stringr::str_subset(string = x, pattern = "^denscrust") %>%
       stringr::str_extract(pattern = "\\d+") %>%
       as.numeric()
   })
-  
+
   col_ids <- map2(pfl_text_data, bedrock_density, function(x, y){
-    
+
     map(x, function(pfl){
       stringr::str_subset(string = pfl, pattern = "Densities") %>%
         str_extract_all(pattern = "\\d+")
@@ -35,19 +47,19 @@ get_well_tops <- function(models_well_location, pfl_files, prm_files, unit_files
         last_bd <- max(which(a == y), na.rm = TRUE) + 3
         3:(last_bd-1)
       })
-  }) 
-  
+  })
+
   # why is this going to NULL?
   out <- pmap(
     .l = list(pfl = pfl_data, well_loc = models_well_location, cols = col_ids),
     function(pfl, well_loc, cols){
-      
+
       row_data <- pmap(list(a = pfl, b = well_loc, c = cols), function(a, b, c){
         a[a$V2 == b, -c]
       })
-      
+
       n_cols <- max(map_dbl(row_data, ncol), na.rm = TRUE)
-      
+
       map_dfr(row_data, function(x){
         x <- as.numeric(x)
         d <- as.data.frame(t(c(x, rep(NA_real_, n_cols - length(x)))))
@@ -58,9 +70,9 @@ get_well_tops <- function(models_well_location, pfl_files, prm_files, unit_files
         relocate(timestep, .before = X)
       }
   )
-  
+
   names(out) <- model_names
-  
+
   out
-  
+
 }
